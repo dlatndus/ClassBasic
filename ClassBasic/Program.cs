ee515148f8464666f2f286695f12aa6289b3f064
Sooyeon Lim <96305365+printyeon@users.noreply.github.com>
2023-06-08 13:59:38 +0900

    클래스명이 겹치는 경우

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -2,10 +2,20 @@
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
+    class Math
+    {
+        public static int abs (int i)
+        {
+            return 0;
+        }
+
+    }
     internal class Program234
     {
         static void Main(string[] args)
         {
+            Math.abs(-3);
+
             OtherClass oc = new OtherClass();
             Console.WriteLine("Hello World!");
         }
